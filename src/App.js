facd7b71395ac7ad77937cb9ca8992164fad2b2b
harshh009 <166378912+harshh009@users.noreply.github.com>
2025-02-01 10:20:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,12 +10,33 @@ export default function App() {
   const [step, setStep] = useState(1);
   const [isOpen, setIsOpen] = useState(true);
 
+
+
+  //BAD WAY TO UPDATE SATTE
+  // function handlePrevious() {
+  //   if (step > 1) setStep(step - 1);
+  // }
+
+  // function handleNext() {
+  //   if (step < 3) setStep(step + 1);
+  // 
+
+  //Good WAy to Updaet State
   function handlePrevious() {
-    if (step > 1) setStep(step - 1);
+    //  if (step > 1) setStep(step - 1);// it works butits bad way always use callback ie\
+    if (step > 1) setStep((s) => s - 1);  // Good wayExplained in below handler function
   }
 
   function handleNext() {
-    if (step < 3) setStep(step + 1);
+    // if (step < 3) setStep(step + 1);  //BAd Way   cuz is we use setStep(step +1) two time in this if condition component will only run setState one time 
+    if (step < 3) setStep((s) => s + 1); //Good WAy  => By thsi approach if we update setStep(step+1) multiple times in if block it all will be execute ie 
+    //if( step <3){
+    //setStep((s)=> s + 1);
+    //setStep((s)=> s + 1);
+  // } this will update step by +2 but  old/bad way only update it once thats  why its bad way and this is the way to update state manually 
+    // 
+// 
+      
   }
 
   return (
